b882bdb7a71ac9a846e99fd97255f6c76e82c4d4
ianmcateer <ian-mcateer55@hotmail.com>
2018-07-16 23:33:53 +1000

    heroku config

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = { mongoURI: mongo_lab_uri }
+  module.exports = { mongoURI: process.env.MONGOLAB_URI }
 } else {
   module.exports = { mongoURI:'mongodb://localhost:27017/gyg-ordering' }
 }
