92ceaa738872feba0fef3a0c39e9106195d0be9e
ianmcateer <ian-mcateer55@hotmail.com>
2018-07-16 13:23:57 +1000

    fixed db, added routers folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 require('dotenv').config()
-
+const menu = require('./routes/api/menu')
 const Item = require('./models/item')
 
 
@@ -11,18 +11,11 @@ const port = process.env.PORT || 3000
 
 // Middleware
 app.use(bodyParser.json())
+app.use('/api/menu', menu)
 
-app.get('/', (req, res) => {
-  res.send(`<h1>Hello working</h1>`)
-})
-
-app.get('/menu', (req, res) => {
-  Item.find().then(
-    items => res.json(items)
-  ).catch(
-    error => res.status(500).json( {error: error.message})
-  )
-})
+// app.get('/', (req, res) => {
+//   res.send(`<h1>Hello working</h1>`)
+// })
 
 mongoose.connect(process.env.MONGOLAB_URI, (err) => {
   if (err) {
