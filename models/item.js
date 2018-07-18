ae0560928731c147f66372b857e3d6018d2e0488
DmanCoder <ktdenis.1@live.com>
2018-07-18 15:45:07 +1000

    Change schema

--- models/item.js
+++ models/item.js
@@ -2,12 +2,23 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Item = new Schema({
-  category: {type: Number},
-  imagePath: {type: String},
-  title: {type: String},
-  description: {type: String},
-  options: {type: Object}, 
-  price: {type: Number}
+  category: String,
+  imagePath: String,
+  title: String,
+  description: String,
+  filling: {
+    type: Boolean, 
+    default: true
+  },
+  heat: {
+    type: Boolean, 
+    default: true
+  },
+  size: {
+    type: Boolean, 
+    default: true
+  },
+  price: Number
 })
 
 module.exports = mongoose.model('Item', Item)
