b1effc531d653b71d60c010d3ccf868b3dcf809e
DmanCoder <ktdenis.1@live.com>
2018-07-25 09:24:05 +1000

    Fixed email

--- routes/api/checkout.js
+++ routes/api/checkout.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // Checkout validations
 const validateCheckoutInput = require('../../validation/checkout')
 
+// Post route
 router.post('/', (req, res) => {
   const body = req.body
   const { errors, isValid } = validateCheckoutInput(body)
@@ -16,32 +17,32 @@ router.post('/', (req, res) => {
   }
 
   // send email
-  const api_key = process.env.mail_gun_api
-  const domain =process.env. mail_gun_domain
-  const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain})
-
+  const api_key = 'e5dbc981cb2e54c89e6d1f9c3c31b9ed-8889127d-26be80dd';
+  const domain = 'sandboxa147aef1091642389d49f9ef0e9c7f6e.mailgun.org';
+  const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+  
   if (!body.company) body.company = 'Company name not provied'
+   
   const data = {
     from: `Guzman y Gomez <${process.env.denis_email}>`,
     to: `${process.env.denis_email}`,
-    subject: 'testing123',
+    subject: 'Third email',
     html: `
       <p><strong>Name</strong>: ${body.firstName} ${body.lastName}</p>
       <p><strong>Phone number</strong>: ${body.phoneNumber}</p>
       <p><strong>Email</strong>: ${body.email}</p>
       <p><strong>Address</strong>: ${body.street} ${body.city}, ${body.postcode}</p>
+      <p><strong>Company</strong>: ${body.company}</p>
+      <hr />
       <p><strong>Orders</strong>: Blank for now</p>
     `
   };
-  
-  mailgun.messages().send(data, function (error, body) {
-    if (error) {
-      console.log(error)
-      res.sendStatus(500)
-    }
+   
+  mailgun.messages().send(data, (error, body) => {
+    if (error) console.log(error)
+    console.log(body);
+  });
 
-    res.send({msg: 'success'})
-
-  })
+  res.send({msg: 'email sent'})
 })
 module.exports = router
